574a5a47e2c4ccbc929a29f58a43840dfc0d47a4
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:40:47 -0800

    add FeaturedSeller entity

---
+++ SabzMarket.DAL/Entities/FeaturedSeller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.DAL.Entities
+{
+    public class FeaturedSeller
+    {
+        public int Id { get; set; }
+        public long SellerId {  get; set; }
+        public virtual Seller? Seller { get; set; }
+        public DateTime StartDate {  get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
