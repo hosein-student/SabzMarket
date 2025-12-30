c0fa2ef980bd9194976bb70cd6c4a4be40014a68
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:45:40 -0800

    add CartItemDTO and FullCartItemDTO

---
+++ SabzMarket.Share/Models/CartItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Share.Models
+{
+    public class CartItemDTO
+    {
+        public int Id { get; set; }
+        public long FarmerId { get; set; }
+        public long ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
---
+++ SabzMarket.Share/Models/FullCartItemDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Share.Models
+{
+    public class FullCartItemDTO
+    {
+        public int Id { get; set; }
+        public long FarmerId { get; set; }
+        public long ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int? ProductPrice { get; set; }
+        public string? ProductImage {  get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedDate { get; set; }
+
+    }
+}
