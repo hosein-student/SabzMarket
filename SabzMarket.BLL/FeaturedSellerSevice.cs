6964e4423c234e2d9e337f29f4bc9b59a4926cde
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:22:00 -0800

    add featured seller business logic and Repositoy

---
+++ SabzMarket.BLL/FeaturedSellerSevice.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using SabzMarket.Share.ErrorHandling;
+using SabzMarket.Share.Mappers;
+using SabzMarket.Share.Models;
+using SabzMarket.Share.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.BLL
+{
+    public class FeaturedSellerSevice : IFeaturedSellerSevice
+    {
+        private readonly IFeaturedSellerRepository _repository;
+        private readonly IErrorService _errorService;
+        public FeaturedSellerSevice(IFeaturedSellerRepository repository, IErrorService errorService) 
+        {
+            _repository = repository;
+            _errorService = errorService;
+        }
+        public async Task<OperationResult<List<SellerFullViewModel>>> GetAllSellerAsync()
+        {
+            var result = await _repository.SelectAllSellerAsync();
+            if (!result.Success)
+            {
+                var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+                var resultError=await _errorService.LogErrorAsync(error);
+                return OperationResult<List<SellerFullViewModel>>.Failed(resultError.Message!.ErrorMessage());
+            }
+            List < SellerFullViewModel > seller =result.Data.Select(x=>x.ToSellerFullViewModel()).ToList(); 
+            return OperationResult<List<SellerFullViewModel>>.SuccessedResult(seller);
+        }
+    }
+}
---
+++ SabzMarket.DAL/FeaturedSellerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Microsoft.EntityFrameworkCore;
+using SabzMarket.DAL.Migrations;
+using SabzMarket.Share.Models;
+using SabzMarket.Share.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.DAL
+{
+    public class FeaturedSellerRepository : IFeaturedSellerRepository
+    {
+        private readonly SabzMarketDbContext _context;
+        public FeaturedSellerRepository(SabzMarketDbContext context) 
+        {
+            _context=context;
+        }
+        public async Task<OperationResult<List<SellerDTO>>> SelectAllSellerAsync()
+        {
+            try
+            {
+                var result = await _context
+                .FeaturedSellers
+                .AsNoTracking()
+                .Select(s =>
+                new SellerDTO()
+                {
+                    Id = s.Seller!.Id,
+                    UserId = s.Seller!.UserId,
+                    ProfileImage = s.Seller.ProfileImage,
+                    FirstName = s.Seller.User!.FirstName,
+                    LastName = s.Seller.User!.LastName,
+                }).ToListAsync();
+                return OperationResult<List<SellerDTO>>.SuccessedResult(result);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<List<SellerDTO>>.Failed(GetType().Name, ex);
+            }
+        }
+    }
+}
