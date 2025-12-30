6a69ff2f342af1fb0efe687d540b72f9a821d457
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 16:41:17 -0800

    add cart item repository and service interface

---
+++ Application/Interfaces/Repositories/ICartItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using SabzMarket.Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface ICartItemRepository
+    {
+        public Task<OperationResult> InsertAsync(CartItemDTO cartItemDTO);
+        public Task<OperationResult> ChangeQuantityAsync(long productId,long farmerId, int number);
+        public Task<OperationResult> DeleteAsync(int cartId);
+        public Task<OperationResult> ExistProductAsync(long farmerId,long productId);
+        public Task<OperationResult> IsCartItemQuantityOneAsync(int id);
+        public Task<OperationResult<List<FullCartItemDTO>>> SelectByFarmerIdAsync(long farmerId);
+
+
+    }
+}
---
+++ Application/Interfaces/Services/ICartItemService.cs
@@ -0,0 +1,17 @@
+﻿using SabzMarket.Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface ICartItemService
+    {
+        public Task<OperationResult> AddToCartAsync(CartItemDTO cartItemDTO);
+        public Task<OperationResult> DecreaseQuantityAsync(long productId,long farmerId);
+        public Task<OperationResult> DeleteAsync(int cartId,long productId,int productNumber);
+        public Task<OperationResult<List<FullCartItemDTO>>> GetByFarmerIdAsync(long farmerId);
+    }
+}
