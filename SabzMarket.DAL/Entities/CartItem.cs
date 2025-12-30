b1cb26ceb9ceb9cd8d826b575529876948b3154b
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:31:11 -0800

    add CartItem entity

---
+++ SabzMarket.DAL/Entities/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.DAL.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public long FarmerId { get; set; }
+        public Farmer Farmer { get; set; } 
+        public long ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
