a48d1c7390ab4dcb52a2bfaaecd8dae9364bfd28
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:15:53 -0800

    implement cart item service logic and Repositoy

---
+++ SabzMarket.BLL/CartItemService.cs
@@ -0,0 +1,120 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using SabzMarket.Share;
+using SabzMarket.Share.ErrorHandling;
+using SabzMarket.Share.Mappers;
+using SabzMarket.Share.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.BLL
+{
+    public class CartItemService : ICartItemService
+    {
+        private readonly ICartItemRepository _repository;
+        private readonly IErrorService _errorService;
+        private readonly IProductOrderDetailHelperService _productOrderDetailHelperService;
+        public CartItemService(
+            ICartItemRepository repository,
+            IErrorService errorService,
+            IProductOrderDetailHelperService productOrderDetailHelperService)
+        {
+            _repository = repository;
+            _errorService = errorService;
+            _productOrderDetailHelperService = productOrderDetailHelperService;
+        }
+        public async Task<OperationResult> AddToCartAsync(CartItemDTO cartItemDTO)
+        {
+            var exist =await _repository.ExistProductAsync(cartItemDTO.FarmerId,cartItemDTO.ProductId);
+            if (!exist.Success)
+            {
+
+                if (!exist.Result)
+                {
+                    var error = exist.Exception!.ExceptionToErrorDTO(exist.Message!);
+                    var errorResult = await _errorService.LogErrorAsync(error);
+                    return OperationResult.Failed(errorResult.Message!.ErrorMessage());
+                }
+
+                var result1 = await _repository.InsertAsync(cartItemDTO);
+                if (!result1.Success)
+                {
+                    var error = result1.Exception!.ExceptionToErrorDTO(result1.Message!);
+                    var errorResult = await _errorService.LogErrorAsync(error);
+                    return OperationResult.Failed(errorResult.Message!.ErrorMessage());
+                }
+                var resultIncrease = await _productOrderDetailHelperService.IncreaseNumberAsync(cartItemDTO.ProductId, -1);
+                if (!resultIncrease.Success)
+                {
+                    return OperationResult.Failed(resultIncrease.Message!);
+                }
+                return OperationResult.SuccessedResult(true, Messages.successAddToCart);
+            }
+
+            var result2 =await _repository.ChangeQuantityAsync(cartItemDTO.ProductId,cartItemDTO.FarmerId, 1);
+            if (!result2.Success)
+            {
+                var error = result2.Exception!.ExceptionToErrorDTO(result2.Message!);
+                var errorResult = await _errorService.LogErrorAsync(error);
+                return OperationResult.Failed(errorResult.Message!.ErrorMessage());
+            }
+            var resultIncrease1 = await _productOrderDetailHelperService.IncreaseNumberAsync(cartItemDTO.ProductId, -1);
+            if (!resultIncrease1.Success)
+            {
+                return OperationResult.Failed(resultIncrease1.Message!);
+            }
+            return OperationResult.SuccessedResult(true, Messages.successAddToCart);
+
+        }
+
+        public async Task<OperationResult> DecreaseQuantityAsync(long productId, long farmerId)
+        {
+            var result2 = await _repository.ChangeQuantityAsync(productId,farmerId ,-1);
+            if (!result2.Success)
+            {
+                var error = result2.Exception!.ExceptionToErrorDTO(result2.Message!);
+                var errorResult = await _errorService.LogErrorAsync(error);
+                return OperationResult.Failed(errorResult.Message!.ErrorMessage());
+            }
+            var resultIncrease = await _productOrderDetailHelperService.IncreaseNumberAsync(productId, 1);
+            if (!resultIncrease.Success)
+            {
+                return OperationResult.Failed(resultIncrease.Message!);
+            }
+            return OperationResult.SuccessedResult(true, Messages.RemoveAddToCart);
+        }
+
+        public async Task<OperationResult> DeleteAsync(int cartId,long productId,int productNumber)
+        {
+            var result2 = await _repository.DeleteAsync(cartId);
+            if (!result2.Success)
+            {
+                var error = result2.Exception!.ExceptionToErrorDTO(result2.Message!);
+                var errorResult = await _errorService.LogErrorAsync(error);
+                return OperationResult.Failed(errorResult.Message!.ErrorMessage());
+            }
+            var resultIncrease = await _productOrderDetailHelperService.IncreaseNumberAsync(productId, productNumber);
+            if (!resultIncrease.Success)
+            {
+                return OperationResult.Failed(resultIncrease.Message!);
+            }
+            return OperationResult.SuccessedResult(true, Messages.RemoveAddToCart);
+        }
+
+        public async Task<OperationResult<List<FullCartItemDTO>>> GetByFarmerIdAsync(long farmerId)
+        {
+            var result=await _repository.SelectByFarmerIdAsync(farmerId);
+            if (!result.Success)
+            {
+                var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+                var resultError= await _errorService.LogErrorAsync(error);
+                return OperationResult<List<FullCartItemDTO>>.Failed(resultError.Message!.ErrorMessage());
+            }
+            return OperationResult<List<FullCartItemDTO>>.SuccessedResult(result.Data);
+        }
+    }
+}
---
+++ SabzMarket.DAL/CartItemRepository.cs
@@ -0,0 +1,135 @@
+﻿using Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using SabzMarket.DAL.Entities;
+using SabzMarket.Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.DAL
+{
+    public class CartItemRepository : ICartItemRepository
+    {
+        private readonly SabzMarketDbContext _Context;
+        public CartItemRepository(SabzMarketDbContext Context) 
+        {
+            _Context = Context;
+        }
+
+        public async Task<OperationResult> DeleteAsync(int cartId)
+        {
+            try
+            {
+                CartItem item = new CartItem()
+                {
+                    Id = cartId
+                };
+                _Context.Remove(item);
+               await _Context.SaveChangesAsync();
+                return OperationResult.SuccessedResult();
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Failed(GetType().Name, ex);
+            }
+
+        }
+
+        public async Task<OperationResult> ExistProductAsync(long farmer,long productId)
+        {
+            try
+            {
+                var result = await _Context
+               .CartItems
+               .AsNoTracking()
+               .Where(x => x.ProductId == productId&&x.FarmerId==farmer)
+               .AnyAsync();
+                return OperationResult.SuccessedResult(result);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Failed(GetType().Name, ex);
+            }
+        }
+           
+        
+
+        public async Task<OperationResult> InsertAsync(CartItemDTO cartItemDTO)
+        {
+            try
+            {
+                CartItem cartItem = new CartItem()
+                {
+                    AddedDate = cartItemDTO.AddedDate,
+                    FarmerId = cartItemDTO.FarmerId,
+                    Id = cartItemDTO.Id,
+                    ProductId = cartItemDTO.ProductId,
+                    Quantity = 1
+                };
+                _Context.Add(cartItem);
+                await _Context.SaveChangesAsync();
+                return OperationResult.SuccessedResult();
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Failed(GetType().Name,ex);
+            }
+        }
+
+        public async Task<OperationResult> ChangeQuantityAsync(long productId,long farmerId, int number)
+        {
+            try
+            {
+                var item =await _Context.CartItems.Where(x => x.ProductId == productId&&x.FarmerId==farmerId).SingleAsync();
+                item.Quantity += number;
+               await _Context.SaveChangesAsync();
+                return OperationResult.SuccessedResult();
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Failed(GetType().Name, ex);
+            }
+
+        }
+
+        public async Task<OperationResult> IsCartItemQuantityOneAsync(int id)
+        {
+            try
+            {
+                var item = await _Context.CartItems.AsNoTracking().Where(x => x.Id == id).SingleAsync();
+                if (item.Quantity == 1)
+                    return OperationResult.SuccessedResult(true);
+                return OperationResult.SuccessedResult(false);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.Failed(GetType().Name,ex);
+            }
+        }
+
+        public async Task<OperationResult<List<FullCartItemDTO>>> SelectByFarmerIdAsync(long farmerId)
+        {
+            try
+            {
+                var result = await _Context.CartItems.AsNoTracking().Where(x => x.FarmerId == farmerId&&x.Product.IsDeleted==false).Select(x => new FullCartItemDTO()
+                {
+                    FarmerId = farmerId,
+                    AddedDate = x.AddedDate,
+                    Id = x.Id,
+                    ProductId = x.ProductId,
+                    ProductImage = x.Product.ImageProduct,
+                    ProductName = x.Product.ProductName,
+                    ProductPrice = x.Product.Price,
+                    Quantity = x.Quantity
+                }).ToListAsync();
+                return OperationResult<List<FullCartItemDTO>>.SuccessedResult(result);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<List<FullCartItemDTO>>.Failed(GetType().Name,ex);
+            }
+        }
+    }
+}
