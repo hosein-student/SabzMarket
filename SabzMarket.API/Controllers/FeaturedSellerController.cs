1834bbaba57f0b5d734c517f926874be640bf2cd
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:08:44 -0800

    add FeaturedSeller API controller

---
+++ SabzMarket.API/Controllers/FeaturedSellerController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using SabzMarket.Share.Models;
+using SabzMarket.Share.ViewModels;
+
+namespace SabzMarket.API.Controllers
+{
+    public class FeaturedSellerController:BaseController
+    {
+        private readonly IFeaturedSellerSevice _featuredSellerSevice;
+        public FeaturedSellerController(IFeaturedSellerSevice featuredSellerSevice)
+        {
+            _featuredSellerSevice = featuredSellerSevice;   
+        }
+        [HttpGet]
+        public async Task<OperationResult<List<SellerFullViewModel>>> GetAllSellerAsync()
+        {
+            var result=await _featuredSellerSevice.GetAllSellerAsync();
+            return result;
+        }
+    }
+}
