40d23422e85c0c48dce0fd20f74322d4906abcef
Hosein <hoseinjaf84ri84@gmail.com>
2025-12-29 17:03:17 -0800

    rename ProductDTO property to Product

--- SabzMarket/UC_Products.cs
+++ SabzMarket/UC_Products.cs
@@ -13,7 +13,7 @@ namespace SabzMarket
 {
     public partial class UC_Products : UserControl
     {
-        public ProductDTO ProductDTO { get; set; }
+        public ProductDTO Product { get; set; }
         public UC_Products()
         {
             InitializeComponent();
@@ -23,21 +23,21 @@ namespace SabzMarket
         public event EventHandler<ProductEventArgs> Edit;
         private void btn_Delete_Click(object sender, EventArgs e)
         {
-            ProductEventArgs product = new ProductEventArgs(ProductDTO);
+            ProductEventArgs product = new ProductEventArgs(Product);
             Delete?.Invoke(this, product);
         }
         private void btn_Edit_Click(object sender, EventArgs e)
         {
-            ProductEventArgs product = new ProductEventArgs(ProductDTO);
+            ProductEventArgs product = new ProductEventArgs(Product);
             Edit?.Invoke(this, product);
         }
 
         private void UC_Products_Load(object sender, EventArgs e)
         {
-            lbl_Name.Text = ProductDTO.Name;
-            lbl_NumberShow.Text = ProductDTO.Number.ToString();
-            lbl_priceShow.Text = ProductDTO.Price.ToString();
-            pb_Image.LoadAsync(ProductDTO.ImageProduct);
+            lbl_Name.Text = Product.Name;
+            lbl_NumberShow.Text = Product.Number.ToString();
+            lbl_priceShow.Text = Product.Price.ToString();
+            pb_Image.LoadAsync(Product.ImageProduct);
         }
 
         private void pb_Image_LoadCompleted(object sender, AsyncCompletedEventArgs e)
