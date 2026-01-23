7658853dd24e16a905207c585a2d9423f2e53672
Hosein <hoseinjaf84ri84@gmail.com>
2026-01-23 20:05:26 +0330

    Add ILogService

---
+++ Application/Interfaces/Logging/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Interfaces.Logging
+{
+    public interface ILogService 
+    {
+        public Task<OperationResult> SaveFailedLogAsync(Exception? ex );
+    }
+}
