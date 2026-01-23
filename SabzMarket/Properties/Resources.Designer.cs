f7cec9ab9334559e0b8c74bd2f664867d93c316c
Hosein <hoseinjaf84ri84@gmail.com>
2026-01-23 18:49:40 +0330

    Change name from SabzMarket to SabzMarket.UI

--- SabzMarket/Properties/Resources.Designer.cs
+++ SabzMarket/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SabzMarket.Properties {
+namespace SabzMarket.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SabzMarket.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SabzMarket.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SabzMarket.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
