cfa2908a5623cf94f9e45904fe268b45d3f2529d
Hosein <hoseinjaf84ri84@gmail.com>
2026-01-30 22:15:40 +0330

    feat(Application): Create UserDTO and UserProfile

---
+++ Application/Users/Mappers/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SabzMarket.Application.Users.DTOs;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Users.Mappers
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User,UserDTO>();
+            CreateMap<UserDTO,User>();
+        }
+    }
+}
